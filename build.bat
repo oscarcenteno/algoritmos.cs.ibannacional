6a223c9f6a0c1be7c35667e246a208abcf43c537
Oscar <oscar.centeno@outlook.com>
2016-05-30 20:00:14 -0600

    Agrego un build.bat para mostrar como se compila el proyecto y se mira los tiempos exactos de ejecucion de cda prueba unitaria.

---
+++ build.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" Algoritmos.CS.IbanNacional.sln
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\mstest.exe" /testcontainer:Algoritmos.CS.IbanNacional.UnitTests\bin\Debug\Algoritmos.CS.IbanNacional.UnitTests.dll /detail:duration
+pause
